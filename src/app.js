55c15bf64331bb3e5292150d23cd3f54f0a0363d
Alexandr <a.platcev@ya.ru>
2023-02-04 00:31:45 +0300

    add test and task2

--- src/app.js
+++ src/app.js
@@ -1,17 +1,4 @@
 import GameSavingLoader from './module/GameSavingLoader';
-import GameSaving from './module/GameSaving';
-import read from './module/reader';
-import json from './module/parser';
 
-// GameSavingLoader.load()
-//   .then((saving) => new GameSaving(JSON.parse(saving)));
-
-// function createSave() {
-//   return (async () => {
-//     try {
-//       const readFile = await read();
-//       const result = await json(readFile);
-//       return result;
-//     } catch (e) { return 'oops'; }
-//   })();
-// }
+console.log(GameSavingLoader.load());
+console.log(GameSavingLoader.asyncLoad());
